3e3764408a5c33ef10f49ac34ac04ae276346eee
227Faddi <falukhouma27@gmail.com>
2024-07-13 04:39:58 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# NextRead : [Visit Here](https://next-read.netlify.app/).
+
+NextRead is a web application designed to help users find book recommendations based on their interests. By entering a topic or genre, users can browse through a selection of books fetched from the [Open Library API](https://openlibrary.org/developers/api). The app displays book titles, authors, and cover images, allowing users to easily discover their next great read.
+
+![NextRead Preview](preview.gif)
+
+## How It's Made:
+
+**Tech used:** HTML, CSS, JavaScript, Bootstrap, [Open Library API](https://openlibrary.org/developers/api).
+
+## Lessons Learned
+During the development of NextRead, I improved my knowledge of API integration and handling various data fetching scenarios, which enhanced my ability to create functional and user-friendly web applications.
+
+## Other Projects:
+Explore more of my recent projects on [My Portfolio](https://faliloukhouma.com).
---
+++ js/main.js
@@ -0,0 +1,83 @@
+const bookContainer = document.querySelector('#bookContainer');
+const bookTitle = document.querySelector('#bookTitle');
+const bookAuthor = document.querySelector('#bookAuthor');
+const bookCover = document.querySelector('#bookCover');
+const noCoverMessage = document.querySelector('#noCoverMessage');
+const spinner = document.querySelector('.spinner-border');
+const bookDetails = document.querySelector('#bookDetails');
+const nextBookBtn = document.querySelector('#nextBookBtn')
+const previousBookBtn = document.querySelector('#previousBookBtn');
+const hidden = document.querySelectorAll('.hidden');
+
+document.querySelector('#getBook').addEventListener('click', getBook);
+
+async function getBook(){
+  try{
+    const userInput = document.querySelector('#userInput').value
+    const url = `https://openlibrary.org/subjects/${userInput}.json`
+    const response = await fetch(url);
+    const data = await response.json();
+    const booksAvailable = data.works.length;
+    
+    function displayBook(index) {
+      bookCover.classList.add('hidden');
+      bookContainer.classList.remove('hidden');
+      spinner.classList.remove('hidden');
+      
+      const coverID = data.works[index].cover_id;
+      if(!coverID){
+        noCoverMessage.classList.remove('hidden');
+        bookCover.classList.add('hidden');
+        spinner.classList.add('hidden');
+      }else{
+        noCoverMessage.classList.add('hidden');
+        bookCover.src = `https://covers.openlibrary.org/b/id/${coverID}.jpg`;
+      }
+      
+      const key = data.works[index].key;
+      bookDetails.href = `https://openlibrary.org${key}`;
+      bookTitle.innerText = data.works[index].title;
+      bookAuthor.innerText = `By ${data.works[index].authors[0].name}`;
+
+      bookCover.onload = function(){
+        // bookContainer.classList.remove('hidden')
+        bookCover.classList.remove('hidden');
+
+        spinner.classList.add('hidden')
+
+        nextBookBtn.classList.remove('hidden')
+        previousBookBtn.classList.remove('hidden')
+      }
+    }
+    
+    // SCROLL THROUGH THE BOOKS AVAILABLE
+    let currentBook = 0;
+    nextBookBtn.addEventListener('click', nextBook)
+    previousBookBtn.addEventListener('click', previousBook)
+  
+    function nextBook() {
+      if (currentBook < booksAvailable - 1) {
+        currentBook++;
+        displayBook(currentBook);
+      } else {
+        currentBook = 0;
+        displayBook(currentBook);
+      }
+    }
+    function previousBook() {
+      if (currentBook > 0) {
+        currentBook--;
+        displayBook(currentBook);
+      } else {
+        currentBook = booksAvailable - 1
+        displayBook(currentBook);
+      }
+    }
+
+    displayBook(currentBook);
+  }
+  catch(err){
+    console.log(err)
+  }
+}
+
